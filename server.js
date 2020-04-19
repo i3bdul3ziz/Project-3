96e461e1c935eb52c81126fea1e6e05d875a1cab
alhaddad <alhaddad-11@hotmail.com>
2020-04-19 19:31:27 +0300

    modls user rout user

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const authRoutes = require('./routs/user')
+
+mongoose.connect('mongodb://localhost/project',{useUnifiedTopology : true , useNewUrlParser : true}  )
+.then(res => console.log("mongodb is connected"))
+.catch(err => console.log(err))
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+
+app.use(authRoutes)
+
+
+ app.get('/' , (req , res) =>{
+
+    res.send("test")
+})
+
+
+
+
+
+
+app.listen(4000 , ()=> console.log('server run on 4000'))
