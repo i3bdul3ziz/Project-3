00e372ddc9bf3a86654742af76f15c4fda988ad2
i7medo <abdulhmeedgh94@gmail.com>
2020-04-23 08:51:11 +0300

    heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT||4000
 
 mongoose
   .connect(
-    "mongodb://localhost/project",
+    process.env.DB,
 
     { useUnifiedTopology: true, useNewUrlParser: true }
   )
